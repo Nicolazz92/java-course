c6bfd4b10c607be122b58b81950d3b625496741b
sbt-velikokhatko-nn <NNVelikokhatko.SBT@sberbank.ru>
2022-03-07 18:30:52 +0300

    -

--- src/main/java/org/velikokhatko/part2/blocking/queue/BlockingQueueExample.java
+++ src/main/java/org/velikokhatko/part2/blocking/queue/BlockingQueueExample.java
@@ -7,62 +7,48 @@ public class BlockingQueueExample {
     public static void main(String[] args) {
         BlockingQueueExample blockingQueueExample = new BlockingQueueExample();
         BlockingQueue blockingQueue = blockingQueueExample.new BlockingQueue();
-        for (int i = 0; i < 50; i++) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+        new Thread(() -> {
+            while (true) {
+                new Thread(blockingQueue.get()).start();
             }
-            boolean offer = blockingQueue.offer(() -> {
-                String uuid = UUID.randomUUID().toString();
-                String taskId = uuid.substring(uuid.lastIndexOf('-') + 1);
-                System.out.printf("Task %s started%n", taskId);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.printf("Task %s finished%n", taskId);
-            });
+        }).start();
+
+        for (int i = 0; i < 10; i++) {
+            boolean offer = blockingQueue.offer(getRunnable());
             if (!offer) {
                 System.out.println("Queue is full");
             }
         }
     }
 
+    private static Runnable getRunnable() {
+        return () -> {
+            String uuid = UUID.randomUUID().toString();
+            String taskId = uuid.substring(uuid.lastIndexOf('-') + 1);
+            System.out.printf("Task %s started%n", taskId);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("Task %s finished%n", taskId);
+        };
+    }
+
     public class BlockingQueue {
-        private static final int THREADS_SIZE = 2;
         private static final int QUEUE_SIZE = 2;
         private final Queue<Runnable> queue = new LinkedList<>();
-        private final Set<Thread> threads = new HashSet<>();
 
-        public BlockingQueue() {
-            new Thread(() -> {
-                while (true) {
-                    get();
-                }
-            }).start();
-        }
-
-        public synchronized void get() {
-            threads.removeIf(thread -> !thread.isAlive() || thread.isInterrupted());
-            if (threads.size() >= THREADS_SIZE) {
-                return;
-            }
+        public synchronized Runnable get() {
             if (queue.isEmpty()) {
                 try {
                     wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            } else {
-                Runnable poll = queue.poll();
-                if (poll != null) {
-                    Thread thread = new Thread(poll);
-                    threads.add(thread);
-                    thread.start();
-                }
             }
+            return queue.poll();
         }
 
         public synchronized boolean offer(Runnable task) {
---
+++ src/main/java/org/velikokhatko/part2/blocking/queue/QqqqExample.java
@@ -0,0 +1,77 @@
+package org.velikokhatko.part2.blocking.queue;
+
+import java.util.*;
+
+public class QqqqExample {
+
+    public static void main(String[] args) {
+        QqqqExample qqqqExample = new QqqqExample();
+        BlockingQueue blockingQueue = qqqqExample.new BlockingQueue();
+        for (int i = 0; i < 50; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean offer = blockingQueue.offer(() -> {
+                String uuid = UUID.randomUUID().toString();
+                String taskId = uuid.substring(uuid.lastIndexOf('-') + 1);
+                System.out.printf("Task %s started%n", taskId);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("Task %s finished%n", taskId);
+            });
+            if (!offer) {
+                System.out.println("Queue is full");
+            }
+        }
+    }
+
+    public class BlockingQueue {
+        private static final int THREADS_SIZE = 2;
+        private static final int QUEUE_SIZE = 2;
+        private final Queue<Runnable> queue = new LinkedList<>();
+        private final Set<Thread> threads = new HashSet<>();
+
+        public BlockingQueue() {
+            new Thread(() -> {
+                while (true) {
+                    get();
+                }
+            }).start();
+        }
+
+        public synchronized void get() {
+            threads.removeIf(thread -> !thread.isAlive() || thread.isInterrupted());
+            if (threads.size() >= THREADS_SIZE) {
+                return;
+            }
+            if (queue.isEmpty()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Runnable poll = queue.poll();
+                if (poll != null) {
+                    Thread thread = new Thread(poll);
+                    threads.add(thread);
+                    thread.start();
+                }
+            }
+        }
+
+        public synchronized boolean offer(Runnable task) {
+            if (queue.size() >= QUEUE_SIZE) {
+                return false;
+            }
+            queue.add(task);
+            notify();
+            return true;
+        }
+    }
+}
