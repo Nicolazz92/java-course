933427a233fbad5d6dd2c19992d07e8939667fbd
sbt-velikokhatko-nn <Nik403512>
2022-03-22 14:49:39 +0300

    -

--- src/main/java/org/velikokhatko/part2/blocking/queue/RealExecutorExample.java
+++ src/main/java/org/velikokhatko/part2/blocking/queue/RealExecutorExample.java
@@ -1,9 +1,7 @@
 package org.velikokhatko.part2.blocking.queue;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.stream.Collectors;
+import java.util.concurrent.*;
 
 import static org.velikokhatko.part2.blocking.queue.Utils.getRunnable;
 
@@ -18,51 +16,59 @@ public class RealExecutorExample {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            realExecutor.execute(getRunnable());
+            try {
+                realExecutor.execute(getRunnable());
+            } catch (RejectedExecutionException e) {
+                System.out.println("Queue if full");
+            }
         }
+        realExecutor.shutDown();
     }
 
     private class RealExecutor implements java.util.concurrent.Executor {
         private static final int THREADS_MAX_SIZE = 2;
         private static final int QUEUE_MAX_SIZE = 2;
         private final Map<Thread, String> threads = new ConcurrentHashMap<>();
-        private final Queue<Runnable> queue = new ConcurrentLinkedQueue<>();
+        private final Set<Worker> workers = new HashSet<>();
+        private final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(QUEUE_MAX_SIZE);
+
+        RealExecutor() {
+            for (int i = 0; i < THREADS_MAX_SIZE; i++) {
+                Worker worker = new Worker();
+                workers.add(worker);
+                threads.put(new Thread(worker), "");
+            }
+            threads.keySet().forEach(Thread::start);
+        }
+
+        public void shutDown() {
+            workers.forEach(Worker::interrupt);
+        }
 
         @Override
-        public void execute(Runnable runnable) {
-            try {
-                if (queue.size() < QUEUE_MAX_SIZE) {
-                    queue.add(runnable);
-                } else {
-                    System.out.println("Queue is full");
-                }
-            } finally {
-                new Thread(this::runNextRunnable).start();
+        public void execute(Runnable runnable) throws RejectedExecutionException, NullPointerException {
+            if (!queue.offer(runnable)) {
+                throw new RejectedExecutionException();
             }
         }
 
-        private void runNextRunnable() {
-            if (!queue.isEmpty()) {
-                if (threads.size() < THREADS_MAX_SIZE) {
-                    Thread newThread = new Thread(queue.poll());
-                    newThread.start();
-                    threads.put(newThread, "");
-                } else {
-                    while (!removeFromCHMap(threads)) {}
-                    runNextRunnable();
+        private final class Worker implements Runnable {
+
+            private volatile boolean isInterrupt = false;
+
+            @Override
+            public void run() {
+                while (!isInterrupt) {
+                    try {
+                        queue.take().run();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-        }
 
-        private boolean removeFromCHMap(Map<Thread, String> threads) {
-            Set<Thread> toRemove = threads.keySet().stream()
-                    .filter(t -> !t.isAlive() || t.isInterrupted())
-                    .collect(Collectors.toSet());
-            if (toRemove.isEmpty()) {
-                return false;
-            } else {
-                toRemove.forEach(threads::remove);
-                return true;
+            public void interrupt() {
+                isInterrupt = true;
             }
         }
     }
