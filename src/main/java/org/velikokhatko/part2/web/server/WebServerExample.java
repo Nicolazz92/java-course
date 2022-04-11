f2d2e5c10adc2cae75984d1958692282772c7351
sbt-velikokhatko-nn <Nik403512>
2022-04-11 17:11:16 +0300

    -

--- src/main/java/org/velikokhatko/part2/web/server/WebServerExample.java
+++ src/main/java/org/velikokhatko/part2/web/server/WebServerExample.java
@@ -9,7 +9,7 @@ public class WebServerExample {
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.printf("Socket is ready to serve on port %d...", PORT);
+            System.out.printf("Socket is ready to serve on port %d...\n", PORT);
             while (true) {
                 new ExtSocketThread(serverSocket.accept()).start();
             }
@@ -21,6 +21,9 @@ public class WebServerExample {
     public static class ExtSocketThread extends Thread {
         Socket clientSocket;
         BufferedReader bufferedReader;
+        InputStream inputStream;
+        OutputStream outputStream;
+        PrintWriter printWriter;
 
         public ExtSocketThread(Socket clientSocket) {
             this.clientSocket = clientSocket;
@@ -29,24 +32,30 @@ public class WebServerExample {
         @Override
         public void run() {
             try {
-                bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                inputStream = clientSocket.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 while (bufferedReader.ready()) {
                     final String line = bufferedReader.readLine();
                     System.out.println(line);
                 }
 
-                try (OutputStream outputStream = clientSocket.getOutputStream();
-                     PrintWriter printWriter = new PrintWriter(outputStream)) {
+                outputStream = clientSocket.getOutputStream();
+                printWriter = new PrintWriter(outputStream);
+                try {
                     printWriter.println("HTTP/1.1 200 Ok");
                     printWriter.println("Content-Type: text/html; charset=utf-8");
                     printWriter.println();
                     printWriter.println("Нормально: " + Thread.currentThread().getId());
                     printWriter.flush();
+                } finally {
+                    tryClose(printWriter);
+                    tryClose(outputStream);
                 }
             } catch (Throwable e) {
                 e.printStackTrace();
             } finally {
                 tryClose(bufferedReader);
+                tryClose(inputStream);
                 tryClose(clientSocket);
             }
         }
@@ -56,6 +65,8 @@ public class WebServerExample {
                 closeable.close();
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                System.out.println(closeable.getClass().getSimpleName() + " closed");
             }
         }
     }
