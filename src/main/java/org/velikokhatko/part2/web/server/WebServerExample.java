2ca6213592c95a444ff8971f431042e2911ebb4c
sbt-velikokhatko-nn <Nik403512>
2022-04-11 16:38:38 +0300

    -

--- src/main/java/org/velikokhatko/part2/web/server/WebServerExample.java
+++ src/main/java/org/velikokhatko/part2/web/server/WebServerExample.java
@@ -3,39 +3,54 @@ package org.velikokhatko.part2.web.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.UUID;
 
 public class WebServerExample {
+    private static final int PORT = 1488;
 
     public static void main(String[] args) {
-        runServer();
-    }
-
-    private static void runServer() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(1488);
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.printf("Socket is ready to serve on port %d...", PORT);
             while (true) {
-                Socket socket = serverSocket.accept();
-                try (final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    while (bufferedReader.ready()) {
-                        final String line = bufferedReader.readLine();
-                        System.out.println(line);
-                    }
-
-                    final PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-                    printWriter.println("HTTP/1.1 200 Ok");
-                    printWriter.println("Content-Type: text/html; charset=utf-8");
-                    printWriter.println();
-                    printWriter.println("Нормально");
-                    printWriter.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (Throwable t) {
-                    t.printStackTrace();
-                    return;
-                }
+                new ExtSocketThread(serverSocket.accept()).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static class ExtSocketThread extends Thread {
+        Socket clientSocket;
+
+        public ExtSocketThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try (final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                while (bufferedReader.ready()) {
+                    final String line = bufferedReader.readLine();
+                    System.out.println(line);
+                }
+
+                try (OutputStream outputStream = clientSocket.getOutputStream();
+                     PrintWriter printWriter = new PrintWriter(outputStream)) {
+                    printWriter.println("HTTP/1.1 200 Ok");
+                    printWriter.println("Content-Type: text/html; charset=utf-8");
+                    printWriter.println();
+                    printWriter.println("Нормально: " + Thread.currentThread().getId());
+                    printWriter.flush();
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
