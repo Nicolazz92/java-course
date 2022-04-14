877b63193591ee6a92bf9762126ff8483acf0c12
sbt-velikokhatko-nn <Nik403512>
2022-04-14 18:28:59 +0300

    -

--- src/main/java/org/velikokhatko/part2/web/server/WebServerExample.java
+++ src/main/java/org/velikokhatko/part2/web/server/WebServerExample.java
@@ -3,11 +3,16 @@ package org.velikokhatko.part2.web.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 public class WebServerExample {
     private static final int PORT = 1488;
 
     public static void main(String[] args) {
+
+        System.out.println((char) 10);
+
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.printf("Socket is ready to serve on port %d...\n", PORT);
             while (true) {
@@ -33,11 +38,32 @@ public class WebServerExample {
         public void run() {
             try {
                 inputStream = clientSocket.getInputStream();
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                while (bufferedReader.ready()) {
-                    final String line = bufferedReader.readLine();
-                    System.out.println(line);
+                int buffSize = 1;
+                byte[] buffer = new byte[buffSize];
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                int realBufferLength;
+                while ((realBufferLength = inputStream.read(buffer)) > -1) {
+                    bytes.write(buffer, 0, realBufferLength);
+                    System.out.println(realBufferLength);
+                    System.out.println(Arrays.toString(buffer));
+                    if (bytes.toString(Charset.defaultCharset()).endsWith(new String(new char[]{10, 13, 10}))) {
+                        System.out.println("ПОЙМАЛ");
+                    }
+                    if (realBufferLength < buffSize || inputStream.available() == -1) {
+                        break;
+                    }
                 }
+                System.out.println(bytes.toString(Charset.defaultCharset()));
+                //TODO https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+                // считать header, вывести их, найти конец хедеров, найти контент-ленгт, считать боди.
+
+//                while (inputStream.available() >= 0)
+//                inputStream.available()
+//                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//                while (bufferedReader.ready()) {
+//                    final String line = bufferedReader.readLine();
+//                    System.out.println(line);
+//                }
 
                 outputStream = clientSocket.getOutputStream();
                 printWriter = new PrintWriter(outputStream);
@@ -48,15 +74,15 @@ public class WebServerExample {
                     printWriter.println("Нормально: " + Thread.currentThread().getId());
                     printWriter.flush();
                 } finally {
-                    tryClose(printWriter);
-                    tryClose(outputStream);
+//                    tryClose(printWriter);
+//                    tryClose(outputStream);
                 }
             } catch (Throwable e) {
                 e.printStackTrace();
             } finally {
-                tryClose(bufferedReader);
-                tryClose(inputStream);
-                tryClose(clientSocket);
+//                tryClose(bufferedReader);
+//                tryClose(inputStream);
+//                tryClose(clientSocket);
                 System.out.println();
             }
         }
