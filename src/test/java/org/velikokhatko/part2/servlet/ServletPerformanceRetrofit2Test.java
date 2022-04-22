519c9048807ec741471c7cef6e8c46db02aa03c4
sbt-velikokhatko-nn <Nik403512>
2022-04-22 11:27:55 +0300

    ServletPerformanceRetrofit2Test.java

---
+++ src/test/java/org/velikokhatko/part2/servlet/ServletPerformanceRetrofit2Test.java
@@ -0,0 +1,58 @@
+package org.velikokhatko.part2.servlet;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ServletPerformanceRetrofit2Test {
+
+    @Test
+    public void doGet() throws InterruptedException {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:8080/java_training_war/")
+                //TODO попрoбовать socket timeout
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        ServletPerformanceTestingApi api = retrofit.create(ServletPerformanceTestingApi.class);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(() -> {
+                try {
+                    ServletPerformanceTestingApi.Resp resp = api.checkPerformance().execute().body();
+                    System.out.println(Objects.requireNonNull(resp).message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executorService.shutdown();
+        Assert.assertTrue(executorService.awaitTermination(100, TimeUnit.SECONDS));
+    }
+
+    private interface ServletPerformanceTestingApi {
+
+        @GET("velikokhatko/servlet/performance")
+        Call<Resp> checkPerformance();
+
+        class Resp {
+            public String message;
+        }
+    }
+}
---
+++ src/test/java/org/velikokhatko/part2/servlet/ServletPerformanceURLConnectionTest.java
@@ -0,0 +1,59 @@
+package org.velikokhatko.part2.servlet;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+//TODO попрoбовать клиент на URLConnection
+public class ServletPerformanceURLConnectionTest {
+
+    @Test
+    public void doGet() throws InterruptedException {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:8080/java_training_war/")
+                //TODO попрoбовать socket timeout
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        ServletPerformanceTestingApi api = retrofit.create(ServletPerformanceTestingApi.class);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(() -> {
+                try {
+                    ServletPerformanceTestingApi.Resp resp = api.checkPerformance().execute().body();
+                    System.out.println(Objects.requireNonNull(resp).message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executorService.shutdown();
+        Assert.assertTrue(executorService.awaitTermination(100, TimeUnit.SECONDS));
+    }
+
+    private interface ServletPerformanceTestingApi {
+
+        @GET("velikokhatko/servlet/performance")
+        Call<Resp> checkPerformance();
+
+        class Resp {
+            public String message;
+        }
+    }
+}
